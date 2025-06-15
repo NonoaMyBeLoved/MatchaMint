6297094e3836c2c4c1c132aae607fe54d3f26abf
NonoaMyBeLoved <samyangchamphi@gmail.com>
2025-06-15 13:39:10 +0900

    Update mission.java

--- RPGTextGame/Map/mission.java
+++ RPGTextGame/Map/mission.java
@@ -16,7 +16,7 @@ public class mission
 
     public void checkLevelUp()
     {
-        if (hero.level == 3 && !message)
+        if(hero.level == 3 && !message)
         {
             System.out.println("****3레벨 달성! 고블린 3마리 처치하면 3번째 기술 해금!****");
             goblinCount = 0;
@@ -26,11 +26,11 @@ public class mission
 
     public void killGoblin()
     {
-        if (hero.level >= 3 && !done)
+        if(hero.level >= 3 && !done)
         {
             goblinCount++;
             System.out.println("현재 잡은 고블린: " + goblinCount + "마리");
-            if (goblinCount >= 3)
+            if(goblinCount >= 3)
             {
                 System.out.println("****미션 완료! 3번째 기술 해금!****");
                 Main.MainRPGTextGame.three = true;
